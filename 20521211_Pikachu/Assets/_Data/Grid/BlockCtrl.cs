fbbcbe8df7bf6d49cb0a8af9f7f3134ed4515a98
Dung261102 <20521211@gm.uit.edu.vn>
2024-10-31 22:40:41 +0700

    Start Project

---
+++ 20521211_Pikachu/Assets/_Data/Grid/BlockCtrl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCtrl : SaiMonoBehaviour
+{
+    //[Header("Block Ctrl")]
+    //public SpriteRenderer spriteRender;
+
+    public SpriteRenderer sprite;
+    //public string blockID = "#";
+    //public BlockData blockData;
+    //public List<BlockCtrl> neighbors = new List<BlockCtrl>();
+
+    //public BlockCtrl Clone()
+    //{
+    //    return (BlockCtrl)this.MemberwiseClone();
+    //}
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadModel();
+        //this.LoadBlockData();
+    }
+
+    protected virtual void LoadModel()
+    {
+        if (this.sprite/*Render*/ != null) return;
+        Transform model = transform.Find("Model");
+        this.sprite/*Render*/ = model.GetComponent<SpriteRenderer>();
+        Debug.Log(transform.name + " LoadModel", gameObject);
+    }
+
+    //protected virtual void LoadBlockData()
+    //{
+    //    if (this.blockData != null) return;
+    //    this.blockData = transform.Find("BlockData").GetComponent<BlockData>();
+    //    Debug.Log(transform.name + " LoadBlockData", gameObject);
+    //}
+
+    //public virtual void SetSprite(Sprite sprite)
+    //{
+    //    this.sprite = sprite;
+    //    this.spriteRender.sprite = sprite;
+    //}
+}
---
+++ 20521211_Pikachu/Assets/_Data/Grid/GridAbstract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public abstract class GridAbstract : SaiMonoBehaviour
+{
+    [Header("Grid Abstract")]
+    public GridManagerCtrl ctrl;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadCtrl();
+    }
+
+    protected virtual void LoadCtrl()
+    {
+        if (this.ctrl != null) return;
+        this.ctrl = transform.parent.GetComponent<GridManagerCtrl>();
+        Debug.LogWarning(transform.name + " LoadCtrl", gameObject);
+    }
+
+
+
+
+}
+
